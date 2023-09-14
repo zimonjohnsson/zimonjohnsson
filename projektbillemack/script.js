e2cb165564ce1b443a1dcb7ba195f4a9b2f7d4f1
zimonjohnsson <zimonjohnsson2004@gmail.com>
2023-09-14 12:58:00 +0200

    kjhgfdsa

--- projektbillemack/script.js
+++ projektbillemack/script.js
@@ -1,3 +1,11 @@
 function showLinks(){
     document.getElementById("navbarlinks").classList.toggle("visa");
 }
+
+function laddaText(){
+    document.getElementById("textbild").classList.add("textladda");
+}
+
+function visalinks(){
+    document.getElementById("otherlinks").classList.toggle("show");
+}
